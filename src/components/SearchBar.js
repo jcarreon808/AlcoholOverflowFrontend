dd6fd5ba5454fa3824b91d9394f97154faf0804b
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-07-30 22:29:07 -0400

    splash page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
-import { Input, Button } from 'semantic-ui-react'
+import { Input, Button, Grid, Segment } from 'semantic-ui-react'
 
 
 export default class SearchBar extends Component {
    render(){
       return(
-        <div className="search-bar">
-          <Input icon='search' placeholder='Search...' onChange={this.props.handleChange} />
-          <Button.Group size='medium'>
-            <Button onClick={this.props.handleFilter} >White</Button>
-            <Button.Or />
-            <Button>Red</Button>
-            <Button.Or />
-            <Button>All</Button>
-          </Button.Group>
-        </div>
+        <Grid columns='equal'>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment><Input icon='search' placeholder='Search...' onChange={this.props.handleChange} /></Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment>
+                <Button.Group size='medium'>
+                  <Button onClick={this.props.handleFilter} >White</Button>
+                  <Button.Or />
+                  <Button>Red</Button>
+                  <Button.Or />
+                  <Button>All</Button>
+                </Button.Group>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       )
    }
 }
