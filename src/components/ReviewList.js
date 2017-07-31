8b823cbde49aa1e0541ed9fd46a415cf081c43f1
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-07-31 14:04:42 -0400

    styling in progress ReviewPage

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -4,9 +4,9 @@ import { List, Grid } from 'semantic-ui-react'
 
 const ReviewList = ({ currentWine }) => {
   return(
-    <div>
+    <div style={{ margin: '0 4em 0 1em' }}>
       <h2>Reviews</h2>
-      <List divided relaxed style={{ height: '275px', overflow: 'scroll'}}>
+      <List divided relaxed style={{ height: '275px', overflowY: 'scroll' }}>
         {currentWine.reviews ? currentWine.reviews.map(review => <Review review={review} />) : null }
       </List>
     </div>
--- src/components/WineCardReviewPage.js
+++ src/components/WineCardReviewPage.js
@@ -4,24 +4,22 @@ import { Image, Grid, Card } from 'semantic-ui-react'
 const WineCardReviewPage = ({ currentWine }) => {
   return(
     <div>
-    <Grid>
-    <Grid.Row columns={2}>
+    <Grid celled verticalAlign='middle' columns={2} centered>
       <Grid.Column style={{ padding: '4em' }}>
         <Image src={`${currentWine.image}`} size='small' />
       </Grid.Column>
       <Grid.Column>
-        <Grid celled textAlign='center' style={{ padding: '1em 0' }} >
+        <Grid textAlign='center' style={{ padding: '1em 0' }} >
           <h4>{currentWine.name}</h4>
           <p>
-            Varietal: {currentWine.varietal}<br/>
-            Region: {currentWine.region}<br/>
-            Winery: {currentWine.winery}<br/>
-            Type: {currentWine.wine_type}<br/>
+            <b><i>Varietal:</i></b> {currentWine.varietal}<br/>
+            <b><i>Region:</i></b> {currentWine.region}<br/>
+            <b><i>Winery:</i></b> {currentWine.winery}<br/>
+            <b><i>Type:</i></b> {currentWine.wine_type}<br/>
           </p>
           <h5>${currentWine.price}</h5>
         </Grid>
     </Grid.Column>
-    </Grid.Row>
     </Grid>
     </div>
   )
